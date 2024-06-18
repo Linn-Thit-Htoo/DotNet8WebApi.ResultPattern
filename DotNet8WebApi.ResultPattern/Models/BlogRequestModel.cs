20cf225ca4c2951baddca0ef742030ba8dc3604e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:47:18 +0630

    -

--- DotNet8WebApi.ResultPattern/Models/BlogRequestModel.cs
+++ DotNet8WebApi.ResultPattern/Models/BlogRequestModel.cs
@@ -1,31 +1,30 @@
-﻿namespace DotNet8WebApi.ResultPattern.Models
+﻿namespace DotNet8WebApi.ResultPattern.Models;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+
+    public Result<BlogResponseModel> IsValid()
     {
-        public string? BlogTitle { get; set; }
-        public string? BlogAuthor { get; set; }
-        public string? BlogContent { get; set; }
+        if (string.IsNullOrEmpty(BlogTitle))
+            return Result<BlogResponseModel>.FailureResult("Blog Title cannot be empty.");
 
-        public Result<BlogResponseModel> IsValid()
-        {
-            if (string.IsNullOrEmpty(BlogTitle))
-                return Result<BlogResponseModel>.FailureResult("Blog Title cannot be empty.");
+        if (string.IsNullOrEmpty(BlogAuthor))
+            return Result<BlogResponseModel>.FailureResult("Blog Author cannot be empty.");
 
-            if (string.IsNullOrEmpty(BlogAuthor))
-                return Result<BlogResponseModel>.FailureResult("Blog Author cannot be empty.");
+        if (string.IsNullOrEmpty(BlogContent))
+            return Result<BlogResponseModel>.FailureResult("Blog Content cannot be empty.");
 
-            if (string.IsNullOrEmpty(BlogContent))
-                return Result<BlogResponseModel>.FailureResult("Blog Content cannot be empty.");
+        return Result<BlogResponseModel>.SuccessResult();
+    }
 
-            return Result<BlogResponseModel>.SuccessResult();
-        }
+    public Result<BlogResponseModel> IsValid(int id)
+    {
+        if (id <= 0)
+            return Result<BlogResponseModel>.FailureResult("Blog Id is invalid.");
 
-        public Result<BlogResponseModel> IsValid(int id)
-        {
-            if (id <= 0)
-                return Result<BlogResponseModel>.FailureResult("Blog Id is invalid.");
-
-            return Result<BlogResponseModel>.SuccessResult();
-        }
+        return Result<BlogResponseModel>.SuccessResult();
     }
 }
