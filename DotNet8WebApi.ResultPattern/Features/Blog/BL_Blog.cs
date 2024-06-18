641c47ae48f612756e19b43699514fd5f2c05f09
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:46:00 +0630

    -

--- DotNet8WebApi.ResultPattern/Features/Blog/BL_Blog.cs
+++ DotNet8WebApi.ResultPattern/Features/Blog/BL_Blog.cs
@@ -1,39 +1,38 @@
 ﻿using DotNet8WebApi.ResultPattern.Models;
 
-namespace DotNet8WebApi.ResultPattern.Features.Blog
+namespace DotNet8WebApi.ResultPattern.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly DA_Blog _dA_Blog;
+
+    public BL_Blog(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
+        _dA_Blog = dA_Blog;
+    }
 
-        public BL_Blog(DA_Blog dA_Blog)
+    public async Task<Result<BlogListResponseModel>> GetBlogList()
+    {
+        return await _dA_Blog.GetBlogList();
+    }
+
+    public async Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel)
+    {
+        return await _dA_Blog.CreateBlog(requestModel);
+    }
+
+    public async Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id)
+    {
+        return await _dA_Blog.UpdateBlog(requestModel, id);
+    }
+
+    public async Task<Result<BlogResponseModel>> DeleteBlog(int id)
+    {
+        if (id <= 0)
         {
-            _dA_Blog = dA_Blog;
+            return Result<BlogResponseModel>.FailureResult("Id is invalid.");
         }
 
-        public async Task<Result<BlogListResponseModel>> GetBlogList()
-        {
-            return await _dA_Blog.GetBlogList();
-        }
-
-        public async Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel)
-        {
-            return await _dA_Blog.CreateBlog(requestModel);
-        }
-
-        public async Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id)
-        {
-            return await _dA_Blog.UpdateBlog(requestModel, id);
-        }
-
-        public async Task<Result<BlogResponseModel>> DeleteBlog(int id)
-        {
-            if (id <= 0)
-            {
-                return Result<BlogResponseModel>.FailureResult("Id is invalid.");
-            }
-
-            return await _dA_Blog.DeleteBlog(id);
-        }
+        return await _dA_Blog.DeleteBlog(id);
     }
 }
