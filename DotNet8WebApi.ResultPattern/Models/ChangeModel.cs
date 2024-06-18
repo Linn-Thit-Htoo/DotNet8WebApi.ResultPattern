8173b2c1089eb1a2cc2bdc9ae67a302d60730ddb
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:47:42 +0630

    -

--- DotNet8WebApi.ResultPattern/Models/ChangeModel.cs
+++ DotNet8WebApi.ResultPattern/Models/ChangeModel.cs
@@ -1,26 +1,25 @@
-﻿namespace DotNet8WebApi.ResultPattern.Models
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Change(this Tbl_Blog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+﻿namespace DotNet8WebApi.ResultPattern.Models;
 
-        public static Tbl_Blog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Change(this Tbl_Blog dataModel)
+    {
+        return new BlogModel
         {
-            return new Tbl_Blog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static Tbl_Blog Change(this BlogRequestModel requestModel)
+    {
+        return new Tbl_Blog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
