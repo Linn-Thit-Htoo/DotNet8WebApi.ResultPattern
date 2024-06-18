1e49b2a3d5c2f9238b1025ba9756b8b733c9f2db
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:48:50 +0630

    -

--- DotNet8WebApi.ResultPattern/ModularService.cs
+++ DotNet8WebApi.ResultPattern/ModularService.cs
@@ -2,51 +2,50 @@
 using DotNet8WebApi.ResultPattern.Features.Blog;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8WebApi.ResultPattern
+namespace DotNet8WebApi.ResultPattern;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
+        services.AddDbContextService(builder);
+        services.AddJsonService();
+        services.AddDataAccessServices();
+        services.AddBusinessLogicServices();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
+        return services;
+    }
+
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
+        return services;
+    }
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContextService(builder);
-            services.AddJsonService();
-            services.AddDataAccessServices();
-            services.AddBusinessLogicServices();
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        });
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    private static IServiceCollection AddJsonService(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(opt =>
         {
-            services.AddScoped<DA_Blog>();
-            return services;
-        }
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>();
-            return services;
-        }
-
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            });
-
-            return services;
-        }
-
-        private static IServiceCollection AddJsonService(this IServiceCollection services)
-        {
-            services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-
-            return services;
-        }
+        return services;
     }
 }
