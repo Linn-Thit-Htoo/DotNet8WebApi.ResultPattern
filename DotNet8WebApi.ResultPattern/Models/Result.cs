de20956ab93cf62eff79833417aea4c5e4dce78b
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:48:01 +0630

    -

--- DotNet8WebApi.ResultPattern/Models/Result.cs
+++ DotNet8WebApi.ResultPattern/Models/Result.cs
@@ -1,29 +1,28 @@
-﻿namespace DotNet8WebApi.ResultPattern.Models
+﻿namespace DotNet8WebApi.ResultPattern.Models;
+
+public class Result<T>
 {
-    public class Result<T>
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public static Result<T> SuccessResult(T data, string message = "Operation Successful.")
     {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
+        return new Result<T> { Success = true, Message = message, Data = data };
+    }
 
-        public static Result<T> SuccessResult(T data, string message = "Operation Successful.")
-        {
-            return new Result<T> { Success = true, Message = message, Data = data };
-        }
+    public static Result<T> SuccessResult(string message = "Operation Successful")
+    {
+        return new Result<T> { Success = true, Message = message };
+    }
 
-        public static Result<T> SuccessResult(string message = "Operation Successful")
-        {
-            return new Result<T> { Success = true, Message = message };
-        }
+    public static Result<T> FailureResult(string message)
+    {
+        return new Result<T> { Success = false, Message = message };
+    }
 
-        public static Result<T> FailureResult(string message)
-        {
-            return new Result<T> { Success = false, Message = message };
-        }
-
-        public static Result<T> FailureResult(Exception ex)
-        {
-            return new Result<T> { Success = false, Message = ex.ToString() };
-        }
+    public static Result<T> FailureResult(Exception ex)
+    {
+        return new Result<T> { Success = false, Message = ex.ToString() };
     }
 }
