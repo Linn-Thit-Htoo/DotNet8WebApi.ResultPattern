7eb3ec9a02b89b1bf72b07ecc9ccfca4fb16e2aa
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:48:37 +0630

    -

--- DotNet8WebApi.ResultPattern/Resources/MessageResource.cs
+++ DotNet8WebApi.ResultPattern/Resources/MessageResource.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNet8WebApi.ResultPattern.Resources
+﻿namespace DotNet8WebApi.ResultPattern.Resources;
+
+public class MessageResource
 {
-    public class MessageResource
-    {
-        public static string Success { get; } = "Success";
-        public static string Fail { get; } = "Fail";
-        public static string SaveSuccess { get; } = "Successfully Save.";
-        public static string SaveFail { get; } = "Fail To Save.";
-        public static string NotFound { get; } = "No Data Found";
-        public static string UpdateSuccess { get; } = "Successfully Update.";
-        public static string UpdateFail { get; } = "Fail To Update.";
-        public static string DeleteSuccess { get; } = "Successfully Delete.";
-        public static string DeleteFail { get; } = "Fail To Delete.";
-        public static string Duplicate { get; } = "Duplicate Data.";
-    }
+    public static string Success { get; } = "Success";
+    public static string Fail { get; } = "Fail";
+    public static string SaveSuccess { get; } = "Successfully Save.";
+    public static string SaveFail { get; } = "Fail To Save.";
+    public static string NotFound { get; } = "No Data Found";
+    public static string UpdateSuccess { get; } = "Successfully Update.";
+    public static string UpdateFail { get; } = "Fail To Update.";
+    public static string DeleteSuccess { get; } = "Successfully Delete.";
+    public static string DeleteFail { get; } = "Fail To Delete.";
+    public static string Duplicate { get; } = "Duplicate Data.";
 }
