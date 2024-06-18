c1a24f7710c5319c7a8331e97fe132c35dfeb350
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-18 13:45:49 +0630

    -

--- DotNet8WebApi.ResultPattern/Database/AppDbContext.cs
+++ DotNet8WebApi.ResultPattern/Database/AppDbContext.cs
@@ -5,9 +5,8 @@ namespace DotNet8WebApi.ResultPattern.Database;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Blog> Blogs { get; set; }
 }
